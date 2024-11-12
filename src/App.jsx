6a61ad177d3a38c930e4618e4f54c632d2c158bf
Rajat Shukla <rajatshukla799@gmail.com>
2024-11-12 18:00:49 +0100

    Added test for rendering object data in JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 function App() {
-  const title = "React";
+  const welcome = {
+    greeting: 'Hello',
+    title: 'React'
+  };
 
   return (
     <div>
-      <h1> Hello {title} ! </h1>
+      <h1> {welcome.greeting} {welcome.title} </h1>
 
       <label htmlFor="Search"> Search : </label>
       <input id="search" type="text" />
