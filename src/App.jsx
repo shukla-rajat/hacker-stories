f22d9e432e010eb4227d9b859649f495a1d5f27a
flash5452 <flash5452@gmail.com>
2024-11-14 17:51:24 +0100

    Implemented JSX rendering for mapped array items

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
-function getTitle(title) {
-  return title;
-}
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 function App() {
   return (
-    <div>
-      <h1> {getTitle("React")} </h1>
+    <>
+      <div>
+        <h1> My Hacker Stories </h1>
 
-      <label htmlFor="Search"> Search : </label>
-      <input id="search" type="text" />
-    </div>
+        <label htmlFor="Search"> Search : </label>
+        <input id="search" type="text" />
+      </div>
+
+      <div>
+        <ul>
+          {list.map((book) => {
+            return <li key={book.objectID}> Title: {book.title} </li>;
+          })}
+        </ul>
+      </div>
+    </>
   );
 }
 
