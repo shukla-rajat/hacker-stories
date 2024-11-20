7b60046108f5d3bf1214a9ac6ddc7fa072197dbd
Rajat Shukla <rajatshukla799@gmail.com>
2024-11-20 14:21:15 +0100

    Extract Item component and introduce props for customization.

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,64 @@
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
 const Search = () => {
-
   const handleChange = (event) => {
-
     console.log(event);
     console.log(event.target.value);
-  }
+  };
 
   return (
     <div>
       <h1> My Hacker Stories </h1>
       <label htmlFor="Search"> Search : </label>
-      <input id="search" type="text" onChange={handleChange}/>
+      <input id="search" type="text" onChange={handleChange} />
     </div>
   );
-}
+};
 
-const List = () => (
+const List = (props) => (
   <ul>
-    {list.map((book) => (
+    {props.list.map((book) => (
       <li key={book.objectID}>
-        <span>Title: {book.title}</span>
-        <span>URL: {book.url}</span>
-        <span>Author: {book.author}</span>
-        <span>Points: {book.points}</span>
+        <Item item={book} />
       </li>
     ))}
   </ul>
 );
 
-const App = () => (
-  <div>
-    <Search />
-    <List />
-  </div>
-);
+const Item = (props) => {
+  return (
+    <div>
+      <span>Title: {props.item.title}</span>
+      <span>URL: {props.item.url}</span>
+      <span>Author: {props.item.author}</span>
+      <span>Points: {props.item.points}</span>
+    </div>
+  );
+};
+
+const App = () => {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+  return (
+    <div>
+      <Search />
+      <List list={stories} />
+    </div>
+  );
+};
 
 export default App;
