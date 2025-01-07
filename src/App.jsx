b269caf442493beb7dffbaf7c88b01b307f0882f
flash5452 <flash5452@gmail.com>
2025-01-07 18:22:09 +0100

    introducing useCallback 'handleFetchStories' and useEffect gets triggered if it changes . Also introducing button to perform explicit data fetching only on submit and not by typing in input box.

--- src/App.jsx
+++ src/App.jsx
@@ -132,12 +132,12 @@ const App = () => {
 
   const [stories, dispatchStories] = React.useReducer(storiesReducer, { data:[], isError: false, isLoading: false });
   const [searchTerm, setSearchTerm] = useStorageState("search", "React");
+  const [url, setUrl] = React.useState(`${API_ENDPOINT}${searchTerm}`);
 
-  React.useEffect(() => {
-    if (!searchTerm) return; 
+  const handleFetchStories = React.useCallback(() => {
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
-    fetch(`${API_ENDPOINT}${searchTerm}`)
+    fetch(url)
       .then((response) => response.json())
       .then((result) => {
         dispatchStories({
@@ -148,15 +148,17 @@ const App = () => {
       .catch(() =>
         dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
       );
-  }, [searchTerm]);
+  }, [url]);
 
-  const handleSearch = (event) => {
+  React.useEffect(() => handleFetchStories(),[handleFetchStories]);
+
+  const handleSearchInput = (event) => {
     setSearchTerm(event.target.value);
   };
 
-  /*const searchedStories = stories.data.filter((story) =>
-    story.title.toLowerCase().includes(searchTerm.toLowerCase())
-  );*/
+  const handleSearchSubmit = () => {
+    setUrl(`${API_ENDPOINT}${searchTerm}`);
+  }
 
   const handleRemoveStories = (item) => {
     dispatchStories({
@@ -170,10 +172,11 @@ const App = () => {
       <InputWithLabel
         id="search"
         value={searchTerm}
-        onInputChange={handleSearch}
+        onInputChange={handleSearchInput}
       >
         <strong>Search : </strong>
       </InputWithLabel>
+      <button type='button' disabled={!searchTerm} onClick={handleSearchSubmit}> Submit </button>
       { stories.isError && <p> Something went wrong </p>}
       { stories.isLoading ? (
         <p>Loading...</p>
