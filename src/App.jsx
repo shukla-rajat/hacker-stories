aed87cb7d2b16e12314dd10a2912e483d8c1dae2
flash5452 <flash5452@gmail.com>
2025-01-14 15:38:26 +0100

    Introduce async/await for improved asynchronous code readability. FIX ERRORS

--- src/App.jsx
+++ src/App.jsx
@@ -135,22 +135,24 @@ const App = () => {
   const [searchTerm, setSearchTerm] = useStorageState("search", "React");
   const [url, setUrl] = React.useState(`${API_ENDPOINT}${searchTerm}`);
 
-  const handleFetchStories = React.useCallback(() => {
+  const handleFetchStories = React.useCallback(async () => {
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
-    axios.get(url)
-      .then((result) => {
-        dispatchStories({
-          type: 'STORIES_FETCH_SUCCESS',
-          payload: result.data.hits,
-        });
-      })
-      .catch(() =>
-        dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
-      );
+    try {
+      const result = await axios.get(url);
+
+      dispatchStories({
+        type: 'STORIES_FETCH_SUCCESS',
+        payload: result.data.hits,
+      });
+    } catch {
+      dispatchStories({ type: 'STORIES_FETCH_FAILURE' });
+    }
   }, [url]);
 
-  React.useEffect(() => handleFetchStories(),[handleFetchStories]);
+  React.useEffect(() => {
+    handleFetchStories();
+  }, [handleFetchStories]);
 
   const handleSearchInput = (event) => {
     setSearchTerm(event.target.value);
