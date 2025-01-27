1a60dab7f47c818e2ec4831ae122c7e116e90401
Rajat Shukla <rajatshukla799@gmail.com>
2025-01-27 17:46:55 +0100

    Add sorting functionality to table: * Added state to track sort order. * Implemented function to handle sorting logic. * Added sort buttons to table headers.

--- src/App.jsx
+++ src/App.jsx
@@ -158,14 +158,36 @@ const InputWithLabel = ({
 };
 
 const List = ({ list, onRemoveItem }) => {
+  const [sort, setSort] = React.useState("NONE");
+
+  const handleSort = (sortKey) => {
+    setSort(sortKey);
+  };
+
   return (
     <ul>
-      <li style={{ display: 'flex' }}>
-        <span style={{ width: '40%' }}> Title </span>
-        <span style={{ width: '30%' }}> Author </span>
-        <span style={{ width: '10%' }}> Comments </span>
-        <span style={{ width: '10%' }}> Points </span>
-        <span style={{ width: '10%' }}> Actions </span>
+      <li style={{ display: "flex" }}>
+        <span style={{ width: "40%" }}>
+          <button type="button" onClick={() => handleSort("TITLE")}>
+            Title
+          </button>
+        </span>
+        <span style={{ width: "30%" }}>
+          <button type="button" onClick={() => handleSort("AUTOR")}>
+            Author
+          </button>
+        </span>
+        <span style={{ width: "10%" }}>
+          <button type="button" onClick={() => handleSort("COMMENT")}>
+            Comments
+          </button>
+        </span>
+        <span style={{ width: "10%" }}>
+          <button type="button" onClick={() => handleSort("POINT")}>
+            Points
+          </button>
+        </span>
+        <span style={{ width: "10%" }}> Actions </span>
       </li>
       {list.map((book) => (
         <Item key={book.objectID} item={book} onRemoveItem={onRemoveItem} />
@@ -176,19 +198,19 @@ const List = ({ list, onRemoveItem }) => {
 
 const Item = ({ item, onRemoveItem }) => {
   return (
-    <li style={{ display: 'flex' }}>
-    <span style={{ width: '40%' }}>
-      <a href={item.url}>{item.title}</a>
-    </span>
-    <span style={{ width: '30%' }}>{item.author}</span>
-    <span style={{ width: '10%' }}>{item.num_comments}</span>
-    <span style={{ width: '10%' }}>{item.points}</span>
-    <span style={{ width: '10%' }}>
-      <button type="button" onClick={() => onRemoveItem(item)}>
-        Dismiss
-      </button>
-    </span>
-  </li>
+    <li style={{ display: "flex" }}>
+      <span style={{ width: "40%" }}>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span style={{ width: "30%" }}>{item.author}</span>
+      <span style={{ width: "10%" }}>{item.num_comments}</span>
+      <span style={{ width: "10%" }}>{item.points}</span>
+      <span style={{ width: "10%" }}>
+        <button type="button" onClick={() => onRemoveItem(item)}>
+          Dismiss
+        </button>
+      </span>
+    </li>
   );
 };
 
