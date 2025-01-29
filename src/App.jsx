cb3afeb8e33cf68e8e9576e22cc8cc54fd5f9052
Rajat Shukla <rajatshukla799@gmail.com>
2025-01-29 12:24:23 +0100

    Extract search term from URL and use as recent search  button title

--- src/App.jsx
+++ src/App.jsx
@@ -2,103 +2,6 @@ import * as React from "react";
 import axios from "axios";
 import { sortBy } from 'lodash';
 
-const App = () => {
-  const [searchTerm, setSearchTerm] = useStorageState("search", "React");
-
-  const [url, setUrl] = React.useState(`${API_ENDPOINT}${searchTerm}`);
-
-  const storiesReducer = (state, action) => {
-    switch (action.type) {
-      case "STORIES_FETCH_INIT":
-        return {
-          ...state,
-          isLoading: true,
-          isError: false,
-        };
-      case "STORIES_FETCH_SUCCESS":
-        return {
-          ...state,
-          isError: false,
-          isLoading: false,
-          data: action.payload,
-        };
-      case "STORIES_FETCH_FAILURE":
-        return {
-          ...state,
-          isError: true,
-          isLoading: false,
-        };
-      case "REMOVE_STORIES":
-        return {
-          ...state,
-          data: state.data.filter((story) => {
-            return story.objectID !== action.payload.objectID;
-          }),
-        };
-      default:
-        throw new Error();
-    }
-  };
-  const [stories, dispatchStories] = React.useReducer(storiesReducer, {
-    data: [],
-    isError: false,
-    isLoading: false,
-  });
-
-  const handleFetchStories = React.useCallback(async () => {
-    dispatchStories({ type: "STORIES_FETCH_INIT" });
-
-    try {
-      const result = await axios.get(url);
-      dispatchStories({
-        type: "STORIES_FETCH_SUCCESS",
-        payload: result.data.hits,
-      });
-    } catch {
-      dispatchStories({ type: "STORIES_FETCH_FAILURE" });
-    }
-  }, [url]);
-
-  React.useEffect(() => {
-    handleFetchStories();
-  }, [handleFetchStories]);
-
-  const handleSearchInput = (event) => {
-    setSearchTerm(event.target.value);
-  };
-
-  const handleSearchSubmit = (event) => {
-    setUrl(`${API_ENDPOINT}${searchTerm}`);
-
-    event.preventDefault();
-  };
-
-  const handleRemoveStories = (item) => {
-    dispatchStories({
-      type: "REMOVE_STORIES",
-      payload: item,
-    });
-  };
-
-  return (
-    <div>
-      <SearchForm
-        searchTerm={searchTerm}
-        onSearchInput={handleSearchInput}
-        onSearchSubmit={handleSearchSubmit}
-      ></SearchForm>
-      {stories.isError && <p> Something went wrong </p>}
-      {stories.isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <List list={stories.data} onRemoveItem={handleRemoveStories} />
-      )}
-    </div>
-  );
-};
-
-const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query=";
-
 const useStorageState = (key, initialState) => {
   const [value, setValue] = React.useState(
     localStorage.getItem(key) || initialState
@@ -111,6 +14,120 @@ const useStorageState = (key, initialState) => {
   return [value, setValue];
 };
 
+const storiesReducer = (state, action) => {
+  switch (action.type) {
+    case "STORIES_FETCH_INIT":
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    case "STORIES_FETCH_SUCCESS":
+      return {
+        ...state,
+        isError: false,
+        isLoading: false,
+        data: action.payload,
+      };
+    case "STORIES_FETCH_FAILURE":
+      return {
+        ...state,
+        isError: true,
+        isLoading: false,
+      };
+    case "REMOVE_STORIES":
+      return {
+        ...state,
+        data: state.data.filter((story) => {
+          return story.objectID !== action.payload.objectID;
+        }),
+      };
+    default:
+      throw new Error();
+  }
+};
+
+const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query=";
+const extractSearchTerm  = (url) => url.replace(API_ENDPOINT,'');
+const getLastSearches = (urls) => urls.slice(-5).map((url) => extractSearchTerm(url));
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useStorageState("search", "React");
+
+  const [urls, setUrls] = React.useState([`${API_ENDPOINT}${searchTerm}`]);
+  const lastSearches = getLastSearches(urls);
+
+  const [stories, dispatchStories] = React.useReducer(storiesReducer, {
+    data: [],
+    isError: false,
+    isLoading: false,
+  });
+
+  const handleFetchStories = React.useCallback(async () => {
+    dispatchStories({ type: "STORIES_FETCH_INIT" });
+
+    try { 
+      const lastUrl = urls[urls.length-1]
+      const result = await axios.get(lastUrl);
+      dispatchStories({
+        type: "STORIES_FETCH_SUCCESS",
+        payload: result.data.hits,
+      });
+    } catch {
+      dispatchStories({ type: "STORIES_FETCH_FAILURE" });
+    }
+  }, [urls]);
+
+  React.useEffect(() => {
+    handleFetchStories();
+  }, [handleFetchStories]);
+
+  const handleSearchInput = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSearchSubmit = (event) => {
+    const url = `${API_ENDPOINT}${searchTerm}`;
+    setUrls(urls.concat(url));
+
+    event.preventDefault();
+  };
+
+  const handleRemoveStories = (item) => {
+    dispatchStories({
+      type: "REMOVE_STORIES",
+      payload: item,
+    });
+  };
+
+  const handleLastSearch = () => {
+    
+  }
+
+  return (
+    <div>
+      <SearchForm
+        searchTerm={searchTerm}
+        onSearchInput={handleSearchInput}
+        onSearchSubmit={handleSearchSubmit}
+      ></SearchForm>
+      {
+        lastSearches.map((url) => (
+          <button type='button' id={url} onClick={() => handleLastSearch(url)}>
+            {url}
+          </button>
+        ))
+      }
+      {stories.isError && <p> Something went wrong </p>}
+      {stories.isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <List list={stories.data} onRemoveItem={handleRemoveStories} />
+      )}
+    </div>
+  );
+};
+
 const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
   <form onSubmit={onSearchSubmit}>
     <InputWithLabel
@@ -120,7 +137,7 @@ const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
     >
       <strong>Search : </strong>
     </InputWithLabel>
-    <button type="button" disabled={!searchTerm}>
+    <button type="submit" disabled={!searchTerm}>
       {" "}
       Submit{" "}
     </button>
